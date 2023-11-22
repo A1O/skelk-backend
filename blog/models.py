421c54070eca37452b403df66cac43980f36bbe1
Ova <centriukas@gmail.com>
2023-11-22 12:59:01 +0200

    implement post-save hook from User to Profile

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
 
 
 class Post(models.Model):
