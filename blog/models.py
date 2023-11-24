adcc923cb1db19e69fc07c1167329fb5ba184b34
Ova <centriukas@gmail.com>
2023-11-24 15:29:07 +0200

    implement actions Follow and Unfollow

--- blog/models.py
+++ blog/models.py
@@ -6,15 +6,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        user_profile = Profile(user=instance)
-        user_profile.save()
-        user_profile.follows.set([instance.profile.id])
-        user_profile.save()
-
-
 class Post(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -42,3 +33,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
--- blog/views.py
+++ blog/views.py
@@ -11,10 +11,23 @@ def dashboard(request):
 
 
 def profile_list(request):
-    profiles = Profile.objects.all()
+    profiles = Profile.objects.exclude(user=request.user.id)
     return render(request, 'blog/profile_list.html', {'profiles': profiles})
 
 
 def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
+
     profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get('follow')
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
     return render(request, 'blog/profile.html', {'profile': profile})
