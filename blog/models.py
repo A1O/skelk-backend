3a2107eb724a0970da7875c8103bba8a3d733090
Ova <centriukas@gmail.com>
2023-11-24 16:26:04 +0200

    show user's blogs

--- blog/models.py
+++ blog/models.py
@@ -50,3 +50,10 @@ class Blog(models.Model):
     )
     body = models.CharField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.user} "
+            f"({self.created_at:%Y-%m-%d %H:%M}): "
+            f"{self.body[:30]}"
+        )
